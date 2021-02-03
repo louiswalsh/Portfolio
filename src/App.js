d240323fba3f7f5bcefb8d0703ee975178aaa81a
louiswalsh <louievw5@gmail.com>
2021-02-03 10:33:28 -0500

    Add the cycleable effect, prepare to include pages with information

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import './App.css';
-import PortfolioParallax from './components/parallax';
+import { render } from 'react-dom'
+import React, { useState, useCallback } from 'react'
+import { useTransition, animated } from 'react-spring'
+import './App.css'
+
+const pages = [
+  ({ style }) => <animated.div class='firstPage' style={{ ...style, background: 'darkblue' }}>A</animated.div>,
+  ({ style }) => <animated.div style={{ ...style, background: 'lightblue' }}>B</animated.div>,
+  ({ style }) => <animated.div style={{ ...style, background: 'lightgreen' }}>C</animated.div>,
+]
 
 function App() {
+  const [index, set] = useState(0)
+  const onClick = useCallback(() => set(state => (state + 1) % 3), [])
+  const transitions = useTransition(index, p => p, {
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  })
   return (
-    <div className="App">
-      <PortfolioParallax></PortfolioParallax>
+    <div className="simple-trans-main" onClick={onClick}>
+      {transitions.map(({ item, props, key }) => {
+        const Page = pages[item]
+        return <Page key={key} style={props} />
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
